f4ddeec509d22374a5102bcd8156acede952244a
cdl08001 <calin.lewis@gmail.com>
2018-06-11 16:16:14 -0700

    Complete getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, node) {
-  node = node || document.body;
+var getElementsByClassName = function(className) {
+  node = document.body;
   var results = [];
   
-  // Check to see if className matches node's class name
-  if(className === node.className){
-  //  - if it does, push to results
-    results.push(node);
-  }
-  
-  // Check and see if node has children:
-  if(node.children){
-  // If it does, run 'getElementsByClassName' on all children
-    // node.children.forEach(function(child){
+  checkClassName(node);
+
+  // for(var i = 0; i < node.children.length; i++){
+  //   checkClassName(node.children[i]);
+  // } 
+
+  function checkClassName(childNode){
+    // Check to see if className matches node's class name
+    if(childNode.className.indexOf(className) != -1){
+      //  - if it does, push to results
+      results.push(childNode);
+      //console.log(results);
+    }
+
+    // Check and see if childNode has children:
+    if(childNode.children.length > 0){
+    // If it does, run 'getElementsByClassName' on all children
+    // childNode.children.forEach(function(child){
     //   getElementsByClassName(className, child);
     // });
-    for(var i = 0; i < node.children.length; i++){
-      getElementsByClassName(className, node.children[i]);
+      for(var i = 0; i < childNode.children.length; i++){
+        checkClassName(childNode.children[i]);
+      }
     }
-  }
 
+  }
+  console.log(results);
   return results; 
 };
-
-
