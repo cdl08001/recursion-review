6eab4f363bf8066b5f15c5fa5c68aa316dd40d41
cdl08001 <calin.lewis@gmail.com>
2018-06-11 12:30:13 -0700

    getElementByClassName.js
    Working commit.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  node = node || document.body;
+  var results = [];
+  
+  // Check to see if className matches node's class name
+  if(className === node.className){
+  //  - if it does, push to results
+    results.push(node);
+  }
+  
+  // Check and see if node has children:
+  if(node.children){
+  // If it does, run 'getElementsByClassName' on all children
+    // node.children.forEach(function(child){
+    //   getElementsByClassName(className, child);
+    // });
+    for(var i = 0; i < node.children.length; i++){
+      getElementsByClassName(className, node.children[i]);
+    }
+  }
+
+  return results; 
 };
+
+
