d112b6a9964b7adbd5ef64f79fb28b92152155b7
cdl08001 <calin.lewis@gmail.com>
2018-06-11 21:56:40 -0700

    Working stringifyJSON.js
    Working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,64 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } 
+    var arr = [];
+    for (var element of obj) {
+      if (typeof element === 'number') {
+        arr.push(element);
+      } else {
+        arr.push(stringifyJSON(element));
+      }
+    }
+    return '[' + arr + ']';
+  } else if (typeof obj !== 'number') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      var object = {};
+      for (var element of Object.keys(obj)) {
+        var key = stringifyJSON(element);
+        var value = stringifyJSON(obj[element]);
+        object[key] = value;          
+      }        
+    }
+    return object;
+  }
+
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
 };
+
+
+
+
+/*
+        if (typeof element === 'string') {
+          object[element] = obj[element];
+        } else {
+          var key = stringifyJSON(element);
+          object[key] = obj[element];          
+        }  
+*/
+
+
+
+/*else {
+      return '[' + obj + ']';
+    }
+*/
