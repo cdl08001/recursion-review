e5d9b5723e13239df499e8908121615cd263407c
Deepali Garg <hdgarg@gmail.com>
2018-06-11 23:47:27 -0700

    Completes successfully stringifyJSON and GetElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function (className) {
+  node = document.body;
+  var results = []; 
+
+  var checkClassName = function (childNode) {
+    // Check to see if className matches node's class name
+    if (childNode.className.indexOf(className) !== -1) {
+      // If it does, push to results
+      results.push(childNode);
+    }
+
+    // Check and see if childNode has children:
+    if (childNode.children.length > 0) {
+      // If it does, run 'checkClassName' on all children
+      for (var i = 0; i < childNode.children.length; i++) {
+        checkClassName(childNode.children[i]);
+      }
+    }
+  };
+  checkClassName(node);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,68 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var arr = [];
+    for (var element of obj) {
+      if (typeof element === 'number') {
+        arr.push(element);
+      } else {
+        arr.push(stringifyJSON(element));
+      }
+    }
+    return '[' + arr + ']';
+  } else if (obj && typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      var result = [];
+      for (var key in obj) {
+        if (obj[key] === undefined || typeof obj[key] === 'function') {
+          continue;
+        } else {
+          result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      }
+    }
+    return '{' + result.join() + '}';
+  }
+
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
 };
+
+
+
+
+/*
+        if (typeof element === 'string') {
+          object[element] = obj[element];
+        } else {
+          var key = stringifyJSON(element);
+          object[key] = obj[element];          
+        }  
+*/
+
+
+
+/*else {
+      return '[' + obj + ']';
+    }
+*/
